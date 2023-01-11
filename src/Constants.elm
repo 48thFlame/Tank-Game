14b281965a45c7c8029bdd775cdbe2ed2c2da410
48thFlame <79379047+48thFlame@users.noreply.github.com>
2023-01-11 18:05:15 +0200

    Added `Constants.elm`

---
+++ src/Constants.elm
@@ -0,0 +1,26 @@
+module Constants exposing (..)
+
+
+tankSpeed : Float
+tankSpeed =
+    140
+
+
+tankRotationSpeed : Float
+tankRotationSpeed =
+    180
+
+
+tankCollisionFriendliness : Float
+tankCollisionFriendliness =
+    8
+
+
+width : String
+width =
+    "780"
+
+
+height : String
+height =
+    "640"
